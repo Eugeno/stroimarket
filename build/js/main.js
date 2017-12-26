8612013c6054016ef5aff4f36c4978a4089be296
eugeno <eugeneath@yandex.ru>
2017-12-26 11:25:19 +0700

    divide step-4

--- build/js/main.js
+++ build/js/main.js
@@ -1,21 +1,22 @@
 'use strict';
 
-var showFilePreview = function showFilePreview(input) {
-  if (input.files && input.files[0]) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-      input.nextElementSibling.style.backgroundImage = 'url(' + e.target.result + ')';
-      input.classList.add('_is-chosen');
-    };
-    reader.readAsDataURL(input.files[0]);
+var filePreview = {
+  show: function show(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        input.nextElementSibling.style.backgroundImage = 'url(' + e.target.result + ')';
+        input.classList.add('_is-chosen');
+      };
+      reader.readAsDataURL(input.files[0]);
+    }
+  },
+  remove: function remove(id) {
+    var input = document.getElementById(id);
+    input.value = '';
+    input.classList.remove('_is-chosen');
+    input.nextElementSibling.style.backgroundImage = 'none';
   }
-};
-
-var removeFilePreview = function removeFilePreview(id) {
-  var input = document.getElementById(id);
-  input.value = '';
-  input.classList.remove('_is-chosen');
-  input.nextElementSibling.style.backgroundImage = 'none';
 };
 
 var ready = function ready() {};
