36aaec53440828dbddd8ebe13dd64e690c753bdb
eugeneath <eugeneath@yandex.ru>
2017-12-27 21:39:44 +0700

    render operating time, save, edit

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 var ready = function ready() {};
 
 document.addEventListener('DOMContentLoaded', ready);
@@ -32,12 +34,64 @@ var modal = {
     modalOverlay.onclick = modalClose.onclick = modal.close;
     document.body.classList.add('_is-fixed');
   },
-  close: function close(event) {
-    var modalWindow = event.target.parentNode;
-    while (!modalWindow.classList.contains('modal')) {
-      modalWindow = modalWindow.parentNode;
+  close: function close(event, selector) {
+    var modalWindow = void 0;
+    if (selector) {
+      modalWindow = document.querySelector(selector);
+    } else {
+      modalWindow = event.target.parentNode;
+      while (!modalWindow.classList.contains('modal')) {
+        modalWindow = modalWindow.parentNode;
+      }
     }
     modalWindow.classList.remove('_is-active');
     document.body.classList.remove('_is-fixed');
+  }
+};
+
+var operatingTime = {
+  save: function save() {
+    var operatingTimeWrapper = document.querySelector('.operating-time-wrapper');
+    var operatingTimeTable = void 0;
+    if (operatingTimeWrapper.hasChildNodes()) {
+      operatingTimeTable = operatingTimeWrapper.querySelector('.registration__operating-time');
+    } else {
+      var operatingTimeTemplate = document.getElementById('operating-time-template');
+      var operatingTimeToClone = operatingTimeTemplate.content.querySelector('.registration__operating-time');
+      operatingTimeTable = operatingTimeToClone.cloneNode(true);
+    }
+
+    var operatingTimeInputTable = document.getElementById('operating-time');
+    var inputs = [].concat(_toConsumableArray(operatingTimeInputTable.querySelectorAll('input')));
+    var th = [].concat(_toConsumableArray(operatingTimeTable.querySelectorAll('th')));
+    inputs.forEach(function (input) {
+      var span = operatingTimeTable.querySelector('[data-id="' + input.dataset.id + '"]');
+      var value = input.value.trim();
+      span.innerHTML = value;
+      var number = input.dataset.id.substr(input.dataset.id.length - 1);
+      if (!value) {
+        span.parentNode.classList.add('_is-empty');
+        if (input.dataset.id.includes('work')) {
+          th[number - 1].classList.add('day-off');
+        }
+      } else {
+        span.parentNode.classList.remove('_is-empty');
+        th[number - 1].classList.remove('day-off');
+      }
+    });
+
+    if (!operatingTimeWrapper.hasChildNodes()) {
+      operatingTimeWrapper.appendChild(operatingTimeTable);
+
+      var setBtn = document.getElementById('set-operating-time');
+      setBtn.remove();
+
+      var editBtnTemplate = document.getElementById('edit-operating-time');
+      var editBtnToClone = editBtnTemplate.content.querySelector('button');
+      var editBtn = editBtnToClone.cloneNode(true);
+      operatingTimeWrapper.appendChild(editBtn);
+    }
+
+    modal.close(null, '#operating-time');
   }
 };
--- js/main.js
+++ js/main.js
@@ -34,12 +34,64 @@ const modal = {
     document.body.classList.add(`_is-fixed`)
   },
 
-  close (event) {
-    let modalWindow = event.target.parentNode
-    while (!modalWindow.classList.contains(`modal`)) {
-      modalWindow = modalWindow.parentNode
+  close (event, selector) {
+    let modalWindow
+    if (selector) {
+      modalWindow = document.querySelector(selector)
+    } else {
+      modalWindow = event.target.parentNode
+      while (!modalWindow.classList.contains(`modal`)) {
+        modalWindow = modalWindow.parentNode
+      }
     }
     modalWindow.classList.remove(`_is-active`)
     document.body.classList.remove(`_is-fixed`)
+  }
+}
+
+const operatingTime = {
+  save () {
+    const operatingTimeWrapper = document.querySelector(`.operating-time-wrapper`)
+    let operatingTimeTable
+    if (operatingTimeWrapper.hasChildNodes()) {
+      operatingTimeTable = operatingTimeWrapper.querySelector(`.registration__operating-time`)
+    } else {
+      const operatingTimeTemplate = document.getElementById(`operating-time-template`)
+      const operatingTimeToClone = operatingTimeTemplate.content.querySelector(`.registration__operating-time`)
+      operatingTimeTable = operatingTimeToClone.cloneNode(true)
+    }
+
+    const operatingTimeInputTable = document.getElementById(`operating-time`)
+    const inputs = [...operatingTimeInputTable.querySelectorAll(`input`)]
+    const th = [...operatingTimeTable.querySelectorAll(`th`)]
+    inputs.forEach((input) => {
+      const span = operatingTimeTable.querySelector(`[data-id="${input.dataset.id}"]`)
+      const value = input.value.trim()
+      span.innerHTML = value
+      const number = input.dataset.id.substr(input.dataset.id.length - 1)
+      if (!value) {
+        span.parentNode.classList.add(`_is-empty`)
+        if (input.dataset.id.includes(`work`)) {
+          th[number - 1].classList.add(`day-off`)
+        }
+      } else {
+        span.parentNode.classList.remove(`_is-empty`)
+        th[number - 1].classList.remove(`day-off`)
+      }
+    })
+
+    if (!operatingTimeWrapper.hasChildNodes()) {
+      operatingTimeWrapper.appendChild(operatingTimeTable)
+
+      const setBtn = document.getElementById(`set-operating-time`)
+      setBtn.remove()
+
+      const editBtnTemplate = document.getElementById(`edit-operating-time`)
+      const editBtnToClone = editBtnTemplate.content.querySelector(`button`)
+      const editBtn = editBtnToClone.cloneNode(true)
+      operatingTimeWrapper.appendChild(editBtn)
+    }
+
+    modal.close(null, `#operating-time`)
   }
 }
